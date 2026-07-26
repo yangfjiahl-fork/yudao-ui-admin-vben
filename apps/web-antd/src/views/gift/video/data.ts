78cd221bec0be1c70f8054ae712d77e4dccbfacc
yangfjiahl <383846707@qq.com>
2026-07-26 18:21:36 +0800

    feat: save

--- apps/web-antd/src/views/gift/video/data.ts
+++ apps/web-antd/src/views/gift/video/data.ts
@@ -177,7 +177,7 @@ export function useGridColumns(): VxeTableGridOptions<GiftVideoApi.Video>['colum
     {
       field: 'createTime',
       title: '创建时间',
-      minWidth: 120,
+      minWidth: 150,
       formatter: 'formatDateTime',
     },
     {
