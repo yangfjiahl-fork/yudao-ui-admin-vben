8bd4130b5849641814e4abb2d1ac175d94d705a8
yangfjiahl <383846707@qq.com>
2026-09-23 19:10:42 +0800

    fix(@vben/request): increase default timeout to 60 seconds

--- packages/effects/request/src/request-client/request-client.ts
+++ packages/effects/request/src/request-client/request-client.ts
@@ -63,7 +63,7 @@ class RequestClient {
       },
       responseReturn: 'raw',
       // 默认超时时间
-      timeout: 30_000,
+      timeout: 60_000,
       paramsSerializer: 'repeat',
     };
     const { ...axiosConfig } = options;
